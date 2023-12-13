0393e8f7c854fe81462fee02af95a0f91c5eea70
Juan Pablo Vercesi <jotapeve@gmail.com>
2023-12-13 19:29:03 -0300

    Add TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import app from './app/app';
+import databaseConnection from './database/connection';
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
-app.listen(PORT);
+databaseConnection.then(() => {
+  console.log('Connected to database');
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(console.error);
