8bfdc844a94baa4b76779e332506e33cdba50f46
Juan Pablo Vercesi <jotapeve@gmail.com>
2023-12-14 13:36:21 -0300

    Improve routing

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -1,9 +1,31 @@
 import { Context } from 'koa'
 import * as HttpStatus from 'http-status-codes';
 
-export default {
+class UserController {
   async index(ctx: Context) {
     ctx.body = [];
     ctx.status = HttpStatus.StatusCodes.OK;
   }
+
+  async create(ctx: Context) {
+    ctx.body = {};
+    ctx.status = HttpStatus.StatusCodes.CREATED;
+  }
+
+  async show(ctx: Context) {
+    ctx.body = {};
+    ctx.status = HttpStatus.StatusCodes.OK;
+  }
+
+  async update(ctx: Context) {
+    ctx.body = {};
+    ctx.status = HttpStatus.StatusCodes.OK;
+  }
+
+  async destroy(ctx: Context) {
+    ctx.body = {};
+    ctx.status = HttpStatus.StatusCodes.NO_CONTENT;
+  }
 }
+
+export default new UserController();
--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -8,5 +8,10 @@ const routerOpts: Router.IRouterOptions = {
 const router: Router = new Router(routerOpts);
 
 router.get('/', user.index);
+router.get('/:id', user.show);
+router.post('/', user.create);
+router.put('/:id', user.update);
+router.patch('/:id', user.update);
+router.delete('/:id', user.destroy);
 
 export default router;
