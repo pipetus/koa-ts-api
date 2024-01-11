c7bf3be7ee0ddb1147b92f0d538461c9b70dbbb2
Oscar Botteri <oscar.botteri@rocketinsights.com>
2024-01-11 14:59:01 -0300

    add swagger docs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,24 @@
 import Koa from 'koa';
+import { koaSwagger } from 'koa2-swagger-ui';
+import yamljs from 'yamljs';
 import { errorHandler } from './middlewares';
 import router from './router';
 
 const app = new Koa();
+const spec = yamljs.load(`${__dirname}/../openapi.yml`);
 
 // Global middlewares
 app.use(errorHandler);
+
+app.use(
+  koaSwagger({
+    title: 'Test',
+    routePrefix: '/docs', // host at /swagger instead of default /docs
+    swaggerOptions: {
+      spec,
+    },
+  }),
+);
 
 // Register routes
 router(app);
